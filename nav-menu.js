0101c455df405575f038f901efa9843b282f7598
Vlad <iptokmach@gmail.com>
2021-05-06 17:26:28 +0200

    refactor: rewrite condition in checkWidth

--- nav-menu.js
+++ nav-menu.js
@@ -6,30 +6,17 @@ class NavMenu {
         this.navItems = [...this.nav.children]
     }
     init() {
-        // this.addStyles();
-        this.createDropdown();
         this.checkWidth();
     }
-    // Эти стиди и так прописаны в css, чтобы при ините не было скачка
-    // В таком случае дублировать их тут нет смысла
-
-    // addStyles() {
-    //     this.wrapper.style.cssText = `
-    //         display: flex;
-    //         align-items: center;
-    //     `;
-    //     this.nav.style.cssText = `
-    //         display: flex;
-    //         align-items: center;
-    //         white-space: nowrap;
-    //         overflow: hidden;
-    //     `;
-    // }
     checkWidth() {
-        let widthItems = 0;
+        let widthItems = 0, dropdown = 0;
         this.navItems.forEach(li => {
             widthItems += li.offsetWidth;
-            if (widthItems > this.nav.offsetWidth) this.hideItems(li);
+            if (widthItems > this.nav.offsetWidth) {
+                dropdown++;
+                if (dropdown === 1) this.createDropdown();
+                this.hideItems(li);
+            }
         });
     }
     createDropdown() {
@@ -44,24 +31,7 @@ class NavMenu {
         document.querySelector('.dropdown__list').insertAdjacentElement('beforeEnd', item);
     }
 }
-/*
-Для работы скрипта элементу ul и его родителю нужно задать стили, лучше всего в css,
-чтобы страница подгружалась уже с нужными стилями
 
-.parent-ul {
-    display: flex;
-    align-items: center;
-}
-.ul {
-    width: 530px;
-    display: flex;
-    align-items: center;
-    white-space: nowrap;
-    overflow: hidden;
-} 
-*/
-
-// инит
 const nav = new NavMenu({
     wrapper: '.nav__wrapper', // селектор родительского элемента ul
     nav: '.nav__ul', // селектор элемента ul
